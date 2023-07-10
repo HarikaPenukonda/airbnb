c492ff7f93690303ea6d1db64306b2bab7d1d312
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-10 16:10:32 -0500

    importing Navbar component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+
+export default function App(){
+
+    /*
+        Challenge: Build the Navbar component
+    */
+    return(
+        <div>
+            <Navbar/>
+        </div>
+        
+    )
+}
