8bac344f0aaa64b61b3967c612cc357c3e1ce022
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-18 14:25:03 -0500

    display badgetext - sold out

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 export default function Card(props){
+    let badgeText
+    if(props.openSpots === 0){
+        badgeText = "SOLD OUT"   
+    } else if (props.location === "online"){
+        badgeText = "ONLINE"
+    }
+    /*
+        Challenge:
+        1. Display the correct text in the badge based on the logic above
+        2. Only display the badge if badgeText has a value
+    */
     return(
        <div className="card">
-       {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
+       {props.openSpots === 0 && <div className="card--badge">{badgeText}</div>}
             <img src={props.img}
             alt="katie-zaferes"
             className="card--image" 
